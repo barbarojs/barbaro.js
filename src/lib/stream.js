e0ed0c63cdd5233f4d07c527ce74514515b85510
Marco Sangalli <sangalli.marco@gmail.com>
2016-12-29 22:11:19 +0100

    fixing streams

---
+++ src/lib/stream.js
@@ -0,0 +1,18 @@
+import {
+	Subject
+} from 'rxjs';
+
+/**
+ * Stream
+ * {id: 'componentId', data: {id: 'dataId', mykey: 1}}
+ */
+export default class Stream extends Subject {
+
+	filterById(id, dataId) {
+		return this.filter((data) => dataId ? (data.id === id && data.data.id === dataId) : (data.id === id));
+	}
+
+	filterByIds(ids) {
+		return this.filter((data) => ids.includes(data.id));
+	}
+}
