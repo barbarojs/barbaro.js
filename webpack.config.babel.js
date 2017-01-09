eab7f26dcc3147b68f712a774a3d4e38ab69bd90
Marco Sangalli <sangalli.marco@gmail.com>
2017-01-09 11:47:13 +0100

    {feature/karma-tests} fixing massive rx import

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -31,12 +31,10 @@ module.exports = {
 			'', '.jsx', '.js', '.json', '.less'
 		],
 		modulesDirectories: [
-			path.resolve(__dirname, 'src/lib'),
 			path.resolve(__dirname, 'node_modules'),
 			'node_modules'
 		],
 		alias: {
-			components: path.resolve(__dirname, 'src/components'), // used for tests
 			style: path.resolve(__dirname, 'src/style'),
 			'react': 'preact-compat',
 			'react-dom': 'preact-compat'
